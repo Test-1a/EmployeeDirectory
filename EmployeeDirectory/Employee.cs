66187cc5478e6874e0932ca23b6c686743b16d8b
DZM <d.bjorlingh@gmail.com>
2019-10-23 16:47:38 +0200

    Add project files.

---
+++ EmployeeDirectory/Employee.cs
@@ -0,0 +1,59 @@
+﻿namespace EmployeeDirectory
+{
+    internal class Employee
+    {
+        //prop
+        private string name;
+
+        public string Name { get; set; }
+        //public string Name
+        //{
+        //    get
+        //    {
+        //        return name;
+        //    }
+        //    set
+        //    {
+        //        if (value != null)
+        //        {
+        //            name = value;
+        //        }
+        //    }
+        //}
+
+        public int Salary { get; set; }
+
+        public Employee(string name, int salary)  
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Salary: {Salary}";
+        }
+
+        //propfull
+        //private string name;
+
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set { name = value; }
+        //}
+
+
+        //public void SetName(string name)
+        //{
+        //    this.name = name;
+        //}
+
+        //public string GetName()
+        //{
+        //    return this.name;
+        //}
+
+    }
+}
---
+++ EmployeeDirectory/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Payroll payroll = new Payroll();
+
+            payroll.Add("Kalle", 10000);
+            payroll.Add("Nisse", 20000);
+            payroll.Add("Anna", 25000);
+            payroll.Add("Lisa", 18000);
+
+            Console.WriteLine("Enter a new employee, Quit with Q");
+
+            do
+            {
+                string name = Util.AskForString("Name: ");
+                if (name == "Q") break;
+
+                int salary = Util.AskForInt("Salary: ");
+
+                payroll.Add(name, salary);
+
+
+
+            }
+            while (true);
+
+
+            //Employee[] employees = payroll.GetEmployees();
+            //Console.WriteLine(employees[0].Name);
+            //employees[0] = null;
+            //var emp2 = payroll.GetEmployees();
+            //Console.WriteLine(emp2[0].Name);
+
+            foreach (var emp in payroll.GetEmployees())
+            {
+                Console.WriteLine(emp);
+            }
+
+        }
+    }
+}
